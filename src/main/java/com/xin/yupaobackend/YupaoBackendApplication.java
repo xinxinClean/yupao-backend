7614e2c0295e8673e15003c09800604b321f2617
xinxinClean <qqxinxinqwzx@outlook.com>
2023-10-14 10:30:21 +0800

    伙伴匹配第一次提交

---
+++ src/main/java/com/xin/yupaobackend/YupaoBackendApplication.java
@@ -0,0 +1,15 @@
+package com.xin.yupaobackend;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
+public class YupaoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YupaoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xin/yupaobackend/YupaoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xin.yupaobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YupaoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
